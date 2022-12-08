ff35faf7de4dfc76e7263de0915265ca7f53b051
zhangtianwei <1837669410@qq.com>
2022-12-08 18:10:11 +0800

    add one_hot

--- utils.py
+++ utils.py
@@ -9,3 +9,10 @@ def set_soft_gpu(soft_gpu):
                 tf.config.experimental.set_memory_growth(gpu, True)
             logical_gpus = tf.config.experimental.list_logical_devices('GPU')
             print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+
+def one_hot(y, depth=10):
+    import torch
+    y_onehot = torch.zeros(y.shape[0], depth)
+    idx = torch.LongTensor(y).view(-1,1)
+    y_onehot.scatter_(dim=1, index=idx, value=1)
+    return y_onehot
